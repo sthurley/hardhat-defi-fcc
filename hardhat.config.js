6e91d2d4002d6935b9f681e45c4809931a92b9f1
Saul Thurley <saul.thurley@gmail.com>
2023-10-30 19:22:33 +0000

    vm exception procession tx

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,6 +16,8 @@ const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
@@ -43,6 +45,12 @@ module.exports = {
             },
             {
               version: "0.4.19"
+            },
+            {
+              version: "0.6.12"
+            },
+            {
+              version: "0.8.0"
             },
         ],
     },
